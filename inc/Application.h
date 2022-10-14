3845155999bdedd8a53596e4cc87030be43a2daa
YuichiKamishiro <yuichikamishiro@gmail.com>
2022-10-15 00:13:20 +0300

    Added helper func, Updated menu.

--- inc/Application.h
+++ inc/Application.h
@@ -1,26 +1,34 @@
-#pragma once
-
 #include <SQLiteCpp/SQLiteCpp.h>
 
 #include <curses.h>
-
 #include <vector>
 #include <string>
+#include <iostream>
+
+#define SELECT_COLOR_PAIR 1
 
 class Application
 {
 private:
     SQLite::Database db;
-
+    
     bool windowShouldClose = false;
     bool startMenuShouldClose = false;
 
     std::vector<std::string> choicesForMenu =
-    { "quit" };
+    { 
+      "Quit",
+      "Print All",
+      "Insert Values"
+    };
 public:
     Application(std::string);
     
     void init();
+    void quit();
+    std::string selectionTable();
+    void printInfoFromTable(std::string);
+    void insertValuesIntoTable();
     void startMenu();
     void run();
 };
--- src/Application.cpp
+++ src/Application.cpp
@@ -3,6 +3,45 @@
 
 Application::Application(std::string nameOfDatabase) : db(nameOfDatabase) {}
 
+void Application::quit()
+{
+    startMenuShouldClose = 1;
+    windowShouldClose = 1;
+}
+
+void Application::printInfoFromTable(std::string nameOfTable)
+{
+    clear();
+
+    SQLite::Statement query(db, "select * from " + nameOfTable);
+
+    int columnCount = query.getColumnCount();
+
+    int offsetY = 0;
+
+    while(query.executeStep())
+    {
+        for(int i = 0; i < columnCount; ++i)
+        {
+            mvprintw(1 + offsetY, 1, query.getColumn(i));
+            ++offsetY;
+        }
+        ++offsetY;
+    }
+    
+    refresh();
+
+    timeout(-1);
+    getch();
+
+    quit();
+}
+
+void Application::insertValuesIntoTable()
+{
+    
+}
+
 void Application::init()
 {
     initscr();
@@ -10,32 +49,58 @@ void Application::init()
     cbreak();
     noecho();
     keypad(stdscr, 1);
-    timeout(-1);
+    timeout(0);
+    curs_set(0);
+
+    start_color();
 }
 
-void Application::startMenu()
+std::string Application::selectionTable()
 {
     clear();
+    
+    timeout(-1);
 
-    int minElement = 0;
-    int currentElement = minElement;
-    int maxElement = 0;
+    mvprintw(1, 1, "Enter table name: ");
+
+    char *bufStr = new char[64];
+    
+    getstr(bufStr);
 
     timeout(0);
 
+    return bufStr;
+}
+
+
+void Application::startMenu()
+{
+    int minElement = 0;
+    int currentElement = minElement;
+    int maxElement = choicesForMenu.size();
+
+    init_pair(SELECT_COLOR_PAIR, COLOR_BLACK, COLOR_WHITE);
+
     while(!startMenuShouldClose)
-    {  
+    {
         int key = getch();
         
         if(key == KEY_UP && currentElement != minElement) --currentElement;
         if(key == KEY_DOWN && currentElement != maxElement) ++currentElement;
-        
+        if(key == 10 && choicesForMenu[currentElement] == "Quit") quit();
+        if(key == 10 && choicesForMenu[currentElement] == "Print All") printInfoFromTable(selectionTable());
+
         for(int i = 0; i < choicesForMenu.size(); ++i)
         {
-            mvprintw(i, 1, choicesForMenu[i].c_str());
+            if(currentElement == i)
+            {
+                attron(COLOR_PAIR(SELECT_COLOR_PAIR));
+                mvprintw(i, 1, choicesForMenu[i].c_str());
+                attroff(COLOR_PAIR(SELECT_COLOR_PAIR));
+            }
+            else mvprintw(i, 1, choicesForMenu[i].c_str());
         }
     }
-    timeout(-1);
 }
 
 void Application::run()
@@ -47,4 +112,6 @@ void Application::run()
     {
 
     }
+
+    endwin();
 }
